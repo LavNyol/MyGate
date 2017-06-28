4525ca9e9fe877dc75fdb44d537cf1ffb79f5ada
LavNyol <insan.lavneesh@gmail.com>
2017-06-28 18:37:31 +0530

    latest changes

--- temp.py
+++ temp.py
@@ -17,22 +17,31 @@ class Departments_Meta(Resource):
         # Connect to databse
         conn = e.connect()
         # Perform query and return JSON data
-        query = conn.execute("select distinct Applicant from salaries")
+        query = conn.execute("select distinct Applicant from trucks")
         return {'Applicants': [i[0] for i in query.cursor.fetchall()]}
 
 
-# class Departmental_Salary(Resource):
-#     def get(self, department_name):
-#         conn = e.connect()
-#         query = conn.execute("select * from salaries where Department='%s'" % department_name.upper())
-#         # Query the result and get cursor.Dumping that data to a JSON is looked by extension
-#         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-#         return result
-#         # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+class Applicant_Details(Resource):
+    def get(self, applicant_name):
+        conn = e.connect()
+        query = conn.execute("select * from trucks where Applicant=?" , applicant_name)
+        # Query the result and get cursor.Dumping that data to a JSON is looked by extension
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return result
+        # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+class Expiry_Details(Resource):
+    def get(self, year):
+        conn = e.connect()
+        # query = conn.execute("select * from trucks where permit like ('%s%')" % (year))
+        print("select * from trucks where permit like ('?')" % (year))
+        # Query the result and get cursor.Dumping that data to a JSON is looked by extension
+        # result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        # return result
 
 
-# api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
+api.add_resource(Applicant_Details, '/applicants/<string:applicant_name>')
 api.add_resource(Departments_Meta, '/applicants')
+api.add_resource(Expiry_Details, '/license/<string:year>')
 
 if __name__ == '__main__':
     app.run()
